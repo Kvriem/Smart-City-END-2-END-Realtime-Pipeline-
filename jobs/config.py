4c9c820ecffd14c1737f9ba4de17c4d59229d36f
كريم عبدالمنعم <145597061+Kvriem@users.noreply.github.com>
2025-08-27 00:43:09 +0300

    Delete jobs/config.py

--- jobs/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-configurations= {
-    "AWS_ACCESS_KEY_ID": os.getenv("AWS_ACCESS_KEY_ID", "AKIAYPZAWIVEDC37UOPJ"),
-    "AWS_SECRET_ACCESS_KEY": os.getenv("AWS_SECRET_ACCESS_KEY", "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")
-}
